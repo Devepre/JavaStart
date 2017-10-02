572d83bd835159c6f699f0f72ac473caf3709c5a
Devepre <bicycle2life@gmail.com>
2017-10-02 16:16:49 +0300

    adding templates

---
+++ JavaStart/src/lection04/Task03.java
@@ -0,0 +1,13 @@
+package lection04;
+
+/*Напечатайте таблицу умножения на 5. Предпочтительно 
+печататсь 1 х 5 =5, 2 х 5 = 10, а не просто 5, 10 и тд.*/
+
+public class Task03 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ JavaStart/src/lection04/TaskAdditional02.java
@@ -0,0 +1,14 @@
+package lection04;
+
+/*С помощью циклов вывести на экран все простые числа от 1 до 100. 
+Простое число – число которое делится нацело только на единицу 
+или само на себя. Первые простые числа это 2,3,5,7…*/
+
+public class TaskAdditional02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
