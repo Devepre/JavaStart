00c83f20a53863e5b8611643684099d8d1eb62ba
Devepre <bicycle2life@gmail.com>
2017-10-02 15:36:41 +0300

    initial commit

---
+++ JavaStart/src/lection02/Task01.java
@@ -0,0 +1,62 @@
+package lection02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*1.	Написать программу которая считывает 5и значное число
+с клавиатуры и выводит цифры из которого оно состоит. Например:
+54698
+5
+4
+6
+9
+8*/
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter number:");
+		Scanner scanner = new Scanner(System.in);		
+		scanner.useLocale(Locale.US); //. as separator
+		Double inputDigit = scanner.nextDouble();	//for bug try 2.36 value for example  
+				
+		if (inputDigit.equals(-0d)) {
+			System.out.println("-0");
+		} else {
+			if (inputDigit.equals(0d)) {
+				System.out.println("0");
+			} else {
+				double digit = inputDigit;
+				if (digit < 0) {
+					System.out.println("-");
+					digit = - digit;
+				}
+				int digitsCount = (int) Math.log10(digit);	
+				int power;
+				int digitToShow = 0;
+				while (digitsCount >= 0) {
+					power = (int) Math.pow(10, digitsCount);
+					digitToShow = (int) (digit / power);
+					digit %= power;
+					digitsCount--;
+					System.out.println(digitToShow);
+				}					
+				if (digit > 0) {
+					System.out.println(".");					
+				
+					digit = inputDigit < 0 ? -inputDigit : inputDigit;	
+					double check = 0;					
+					double checkInput = inputDigit % 1;	//0.3599999999999999 instead of 0.36 here
+					int i = 0;
+					do {
+						digit *= 10;	//23.599999999999998 instead of 23.6
+						digitToShow = (int)digit % 10;
+						System.out.println(digitToShow);											
+						check +=  (double)digitToShow / (Math.pow(10, ++i)); 								
+					} while (check < checkInput);   //so here we get infinitive loop
+				}				
+			}
+		}	
+	}
+
+}
---
+++ JavaStart/src/lection02/Task02.java
@@ -0,0 +1,41 @@
+package lection02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*Написать программу, которая вычислит и выведет на экран
+площадь треугольника если известны его стороны.*/
+
+public class Task02 {
+
+	public static void main(String[] args) {
+		float a;
+		float b;
+		float c;
+		
+		if (args.length == 0) {
+			System.out.println("Please enter a, b, c describing triangle:");
+			Scanner scanner = new Scanner(System.in);
+			scanner.useLocale(Locale.US);
+			a = scanner.nextFloat();
+			b = scanner.nextFloat();
+			c = scanner.nextFloat();
+			
+		} else {
+			a = Float.parseFloat(args[0]);
+			b = Float.parseFloat(args[1]);
+			c = Float.parseFloat(args[2]);
+		}
+		
+		if (a + b < c || a + c < b || b + c < a) {
+			System.out.println("It's not a triangle!");						
+		} else {
+			double p = (a + b + c) / 2;
+			double square = Math.sqrt(p * (p - a) * (p - b) * (p - c) );
+			
+			System.out.println("Square is: " + square);
+		}		
+
+	}
+
+}
