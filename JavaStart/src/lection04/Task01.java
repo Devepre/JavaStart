dd3a72dd5ef34be363a520110916ae8f1f3203fb
devepr <devepr@devHP>
2017-10-03 20:17:15 +0300

    Solving Lection4, task 1

--- JavaStart/src/lection04/Task01.java
+++ JavaStart/src/lection04/Task01.java
@@ -1,5 +1,7 @@
 package lection04;
 
+import java.util.Scanner;
+
 /*С помощью циклов нарисовать «обои». Причем количество 
  * полос должно вводиться с клавиатуры. В примере 7 полос.
  * ***+++***+++***+++***
@@ -8,7 +10,21 @@ package lection04;
 public class Task01 {
 
 	public static void main(String[] args) {
-		
+		int num;
+		System.out.println("Please enter number of lines");
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		sc.close();
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < num; j++) {
+				if (j % 2 == 0) {
+					System.out.print("***");
+				} else {
+					System.out.print("+++");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }
