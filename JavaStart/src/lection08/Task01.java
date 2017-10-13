7ca2440c659197dbdf09500e92a444b76191237f
Devepre <bicycle2life@gmail.com>
2017-10-13 12:43:14 +0300

    Solving Lection8, task 1

--- JavaStart/src/lection08/Task01.java
+++ JavaStart/src/lection08/Task01.java
@@ -1,13 +1,40 @@
 package lection08;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /*Создайте консольный «текстовый редактор» с 
  * возможностью сохранения набранного текста в файл.*/
 
 public class Task01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String fileName = "new.txt";
+		String terminator = "#save";
 
+		System.out.println("Please provide file name to create");
+		Scanner sc = new Scanner(System.in);
+		fileName = sc.nextLine();
+
+		System.out.println("Excelent! Now please provide your text to save. For saving file print \"" 
+				+ terminator
+				+ "\" line and press Enter");
+
+		File file = new File(fileName);
+		String inputLine = "";
+		try (PrintWriter pw = new PrintWriter(file)) {
+			for (; !inputLine.equals(terminator); inputLine = sc.nextLine()) {
+				inputLine = sc.nextLine();
+				pw.println(inputLine);
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("Done.");
 	}
 
 }
