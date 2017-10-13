a6f43016c26f11f772e9d6e8b9cb0744e8020664
Devepre <bicycle2life@gmail.com>
2017-10-13 13:05:57 +0300

    Solving Lection8, task 3

--- JavaStart/src/lection08/Task03.java
+++ JavaStart/src/lection08/Task03.java
@@ -1,5 +1,8 @@
 package lection08;
 
+import java.io.File;
+import java.util.Scanner;
+
 /*Реализуйте метод который вывведет на экран список всех 
  * каталогов котоорые «лежат» в каталоге который будет 
  * параметром этого метода.*/
@@ -7,8 +10,25 @@ package lection08;
 public class Task03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String fileName = "";
+		System.out.println("Please enter directory path:");
+		Scanner sc = new Scanner(System.in);
+		fileName = sc.nextLine();
+		sc.close();
+		File file = new File(fileName);
+		printDirs(file);
 
 	}
-
+	public static void printDirs(File dir) {
+		if (dir.isDirectory()) {
+			File[] array = dir.listFiles();
+			for (File item : array) {
+				if (item.isDirectory()) {
+					System.out.println(item.getName());
+				}
+			}
+		} else {
+			System.out.println(dir.getName() + " isn't directory");
+		}
+	}
 }
