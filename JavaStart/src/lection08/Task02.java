9c9c432d1d775c8c7176225815536f32c0df131a
Devepre <bicycle2life@gmail.com>
2017-10-13 12:55:37 +0300

    Solving Lection8, task 2

--- JavaStart/src/lection08/Task02.java
+++ JavaStart/src/lection08/Task02.java
@@ -1,13 +1,43 @@
 package lection08;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 /*Напишите метод для сохранения в текстовй файл 
  * двухмерного массива целых чисел.*/
 
 public class Task02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] array = new int[][] { { 2, 5, 2 }, { 67, 34, 75 }, { 0, 1, 2 } };		
+		String fileName = "a.txt";
 
+		System.out.println("Saving array to file " + fileName);
+		if (saveArrayToFile(array, fileName)) {
+			System.out.println("Saved succesfully");
+		} else {
+			System.out.println("Error occured during saving");
+		}
+
+	}
+
+	public static boolean saveArrayToFile(int[][] array, String fileName) {
+		boolean isSuccesfull = true;
+		if (array!= null) {
+			try (PrintWriter pw = new PrintWriter(fileName)) {
+				for (int[] arr : array) {
+					pw.println(Arrays.toString(arr));
+				}
+			} catch (FileNotFoundException e) {
+				isSuccesfull = false;
+				System.out.println(e.getMessage());
+			}			
+		} else {
+			isSuccesfull = false;
+		}
+		
+		return isSuccesfull;
 	}
 
 }
